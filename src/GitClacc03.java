20a23c89a741bd7d1c8dd2a0f2ce8aa98dd83833
OlgaSt684 <olst1207@gmail.com>
2022-04-18 13:50:57 -0700

    forth commit

--- src/GitClacc03.java
+++ src/GitClacc03.java
@@ -2,7 +2,7 @@
 public class GitClacc03 {
 
 	public static void main(String[] args) {
-		System.out.println("This is the first commit file.");
+		System.out.println("This is the not commit file.");
 
 	}
 
